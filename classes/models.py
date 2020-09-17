d8f6888cc6fef21b749bafcb5cbb86c1fcb2b6bb
Vsevalot <v.v.tetervak@gmail.com>
2020-09-17 17:23:54 +0500

    recoded classes

--- classes/models.py
+++ classes/models.py
@@ -1,24 +1,76 @@
 import pygame
-from typing import Tuple
+from typing import Tuple, NewType
 
 
-class model():
-    def __init__(self, path_to_img: str, x:int = 0, y:int = 0):
-        self.img = pygame.image.load(path_to_img)
+class Model():
+    def __init__(self, path_to_img: str, x: int = 0, y: int = 0):
+        self.idle_img = pygame.image.load(path_to_img)
+        self.width, self.height = self.idle_img.get_size()
+        self.frames = {"current": self.idle_img, "IDLE": self.idle_img}
+        self.current_frame = 0
         self.x = x
         self.y = y
+        self.layer = 0  # objects intersect/get collision only if they are one the same layer
         self.x_velocity = 0
         self.y_velocity = 0
+        self.type = ''
+        self.movable = True
 
-    def set_x_velocity(self, velocity: int = 1) -> None:
-        self.x_velocity = 1
+    def set_x_coordinate(self, x: int):
+        self.x = x
 
-    def set_y_velocity(self, velocity: int = 1) -> None:
-        self.y_velocity = 1
+    def set_y_coordinate(self, y: int):
+        self.y = y
 
     def get_coordinates(self) -> Tuple[int, int]:
         return self.x, self.y
 
-    def update_coordinates(self) -> None:
-        self.x += self.x_velocity
-        self.y += self.y_velocity
+    def add_frame(self, path_to_frame, frame_name) -> bool:
+        """
+        Adds frames to the model.
+        :param path_to_frame: path to jpg, png, bmp file with a frame
+        :param frame_name: name of the frame in frame dictionary
+        :return: Return True if frame was added and False if there is a frame with given name
+        """
+        if frame_name in self.frames:
+            return False
+        else:
+            frame = pygame.image.load(path_to_frame)
+            if frame.get_size() != (self.width, self.height):
+                print(f"Warning! The frame {frame_name} size is {frame.get_size()} "
+                      f"while model size is ({self.width}, {self.height})")
+            self.frames[frame_name] = frame
+            return True
+
+    def set_current_frame(self, frame_name: str) -> bool:
+        """
+        Tries to set frame with given name as current frame
+        :param frame_name: Name of the frame in self.frames dictionary
+        :return: True if frame is set False else
+        """
+        if frame_name in self.frames:
+            self.frames["current"] = self.frames[frame_name]
+            return True
+        return False
+
+    def get_current_frame(self) -> str:
+        """
+        Goes through the frame dict and find the name of current frame
+        :return: name of the current frame
+        """
+        for f in self.frames:
+            if self.frames[f] is self.frames["current"]:
+                return f
+
+    def is_movable(self) -> bool:
+        return self.movable
+
+class Player(Model):
+    def __init__(self, path_to_img: str, x: int = 0, y: int = 0):
+        super().__init__(path_to_img, x, y)
+        self.idle_delay = 60
+        self.idle_time = 0
+        self.type = "player"
+
+    def set_idle_delay(self, delay: int):
+        self.idle_delay = delay
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
+from typing import List
 from random import randint
-from classes import models
+from classes.models import Model, Player
+import os
 
 
 SCREEN_WIDTH, SCREEN_HEIGHT = 800, 600
@@ -13,39 +15,74 @@ D_KEY = 100
 A_KEY = 97
 W_KEY = 119
 S_KEY = 115
+UP_KEY = 273
+DOWN_KEY = 274
+RIGHT_KEY = 275
+LEFT_KEY = 276
 PLAYER_STEP = 40
 
 
-def draw_grid(surface: pygame.Surface, surface_x: int, surface_y: int, cell_size: int) -> None:
+def draw_grid(surface: pygame.Surface, surface_width: int, surface_height: int, cell_size: int) -> None:
     """
     Draws grid to make positioning easier
     :param surface: game display
-    :param surface_x: x coordinate size of the display (px)
-    :param surface_y: y coordinate size of the display (px)
+    :param surface_width: max x coordinate size of the display - width (px)
+    :param surface_height: max y coordinate size of the display - height (px)
     :param cell_size: size of a cell on the grid (px)
     :return: None
     """
-    for i in range(surface_x // cell_size):
-        pygame.draw.line(surface, BLACK_RGB, (i * cell_size, 0), (i * cell_size, surface_y))
-    for k in range(surface_y // cell_size):
-        pygame.draw.line(surface, BLACK_RGB, (0, k * cell_size), (surface_x, k * cell_size))
+    for i in range(surface_width // cell_size):
+        pygame.draw.line(surface, BLACK_RGB, (i * cell_size, 0), (i * cell_size, surface_height))
+    for k in range(surface_height // cell_size):
+        pygame.draw.line(surface, BLACK_RGB, (0, k * cell_size), (surface_width, k * cell_size))
 
 
-if __name__ == "__main__":
-    """
-    INITIALIZING  
-    """
-    pygame.init()
-    display: pygame.Surface = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
-    pygame.display.set_caption("TheGreatestGameEver")
-    clock = pygame.time.Clock()
-    clock.tick(FPS)  # setting FPS
-    running = True
+def redraw_game_window(display: pygame.Surface, objects_to_draw: List[Model]):
+    display.fill(WHITE_RGB)
+    draw_grid(display, SCREEN_WIDTH, SCREEN_HEIGHT, CELL_SIZE)
+    for model in objects_to_draw:
+        display.blit(model.frames["current"], model.get_coordinates())  # Draw a surface
+    pygame.display.update()  # This line redraw the scene / object. Same as pygame.display.flip()
 
 
+def prepare_models() -> List[Model]:
     path_to_player_img = "images/definitely_an_imperial_trooper.png"
-    player = models.model(path_to_player_img, x=1, y=1)
+    player = Player(path_to_player_img, x=1, y=81)
+    path_to_player_frames = "images/player_frames"
+    player_frames = [os.path.join(path_to_player_frames, f) for f in os.listdir(path_to_player_frames)]
+    for path_to_frame in player_frames:
+        frame_name = os.path.basename(path_to_frame).split('.')[0]
+        if not player.add_frame(path_to_frame, frame_name):
+            print(f"{frame_name} cannot be added!")
 
+    path_to_rock = "images/rock.bmp"
+    rock = Model(path_to_rock, 1, 201)
+    rock.type = "rock"
+
+    path_to_wall = "images/wall.bmp"
+    wall = Model(path_to_wall, 1, 41)
+    wall.type = "wall"
+    wall.movable = False
+
+    walls = [Model(path_to_wall, 40 * randint(1, 10) + 1, 40 * randint(1, 10) + 1) for i in range(10)]
+    walls = [w for w in walls if w.get_coordinates() not in [m.get_coordinates for m in walls]]  # remove dublicate
+
+    return [player, rock, wall] + walls
+
+
+def run_game_loop(display: pygame.Surface) -> None:
+    """
+    The main game loop
+    :param display: game display from pygame.display.set_mode
+    :return: None
+    """
+    # Setup game loop
+    clock = pygame.time.Clock()
+    running = True
+    
+    models = prepare_models()
+    player: Player = [m for m in models if m.type == "player"][0]  # It's ok, pycharm goes little crazy about subclasses
+    
     while running:
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
@@ -55,22 +92,52 @@ if __name__ == "__main__":
                 """
                 Movement control
                 """
-                if event.key == W_KEY:  # Move up key
+                if (event.key == W_KEY) or (event.key == UP_KEY):  # Move up key
                     if player.y > 1:
-                        player.y -= PLAYER_STEP
-                elif event.key == D_KEY:  # Move right key
+                        player.set_y_coordinate(player.y - PLAYER_STEP)
+                        player.set_current_frame("UP")
+                        player.idle_time = 0
+                        for m in models:  # Checking for collisions
+                            if m is not player:
+                                if player.get_coordinates() == m.get_coordinates():
+                                    player.set_y_coordinate(player.y + PLAYER_STEP)
+                                    if m.is_movable():
+                                        if m.y > 1:
+                                            m.set_y_coordinate(m.y - PLAYER_STEP)
+                                            for 
+                elif (event.key == D_KEY) or (event.key == RIGHT_KEY):  # Move right key
                     pass
-                elif event.key == S_KEY:  # Move down key
+                elif (event.key == S_KEY) or (event.key == DOWN_KEY):  # Move down key
                     if player.y < SCREEN_HEIGHT - PLAYER_STEP:
-                        player.y += PLAYER_STEP
-                elif event.key == A_KEY:  # Move left key
-                    pass
+                        player.set_y_coordinate(player.y + PLAYER_STEP)
+                        player.set_current_frame("DOWN")
+                        player.idle_time = 0
+                        for m in models:  # Checking for collisions
+                            if m is not player:
+                                if player.get_coordinates() == m.get_coordinates():
+                                    player.set_y_coordinate(player.y - PLAYER_STEP)
+                                    if m.is_movable():
+                                        if m.y < SCREEN_HEIGHT - PLAYER_STEP:
+                                            m.set_y_coordinate(m.y + PLAYER_STEP)
 
-            display.fill(WHITE_RGB)
-            draw_grid(display, SCREEN_WIDTH, SCREEN_HEIGHT, CELL_SIZE)
-            display.blit(player.img, player.get_coordinates())
-            pygame.display.update()  # This line redraw the scene / object same as pygame.display.flip()
+                elif (event.key == A_KEY) or (event.key == LEFT_KEY):  # Move left key
+                    pass
             print(f"DEBUG --- {event}")
 
+        player.idle_time += 1
+        if player.idle_time > player.idle_delay:
+            if player.get_current_frame() != "IDLE":
+                player.set_current_frame("IDLE")
+        redraw_game_window(display, models)
+
+        clock.tick(FPS)  # Setting FPS. Don't really know how it works but I can't find any docs in the web
+
+
+if __name__ == "__main__":
+    # Setup display
+    pygame.init()
+    display = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
+    pygame.display.set_caption("TheGreatestGameEver")
+    run_game_loop(display)
     pygame.quit()
     quit(0)
